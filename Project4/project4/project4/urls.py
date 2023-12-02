6872567f0d56d3393bf75ce557e2c235f3330280
UzmaSayyeda <uzmafatima5991@gmail.com>
2023-12-02 12:53:46 -0600

    added style to htmls

--- Project4/project4/project4/urls.py
+++ Project4/project4/project4/urls.py
@@ -20,5 +20,6 @@ from .import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.home)
+    path("", views.home),
+    path("predict/", views.predict)
 ]
--- Project4/project4/project4/views.py
+++ Project4/project4/project4/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def predict(request):
+    return render(request, 'predict.html')
