e40735b032cf4660fc8cec94ddf54c0856645543
Emichi25 <evanmichigan@outlook.com>
2023-12-08 18:21:33 -0600

    Made a rough graph using sqlite. Launch at main.py

--- flask-model/main.py
+++ flask-model/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, jsonify
 import sqlalchemy as sql
+from sqlalchemy import create_engine
+import pandas as pd
 import sqlite3
 
 app = Flask(__name__)
@@ -9,18 +11,28 @@ engine = sql.create_engine(db_url)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    query = "SELECT * FROM drug_effects"
+    data = pd.read_sql(query, engine)
+    drug_data = data.to_dict(orient='records')
+    return render_template("index.html", drug_data=drug_data)
 
 @app.route("/data")
 def get_data():
-    
     conn = sqlite3.connect("drugs.db")
     cursor = conn.cursor()
     query = "SELECT * FROM drug_effects"
-    data = cursor.execute(query).fetchall()
+    data_two = cursor.execute(query).fetchall()
+
+
+    drug_data_two = []
+    for row in data_two:
+        row_dict = {}
+        for i, column in enumerate(cursor.description):
+            row_dict[column[0]] = row[i]
+        drug_data_two.append(row_dict)
+
     conn.close()
-    return jsonify(data)
-    
+    return jsonify(drug_data_two)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- flask-model/static/js/index.js
+++ flask-model/static/js/index.js
@@ -3,3 +3,41 @@ let response = "/data"
 d3.json(response).then(function(data){
     console.log(data)
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("/data")
+      .then((response) => response.json())
+      .then((data) => {
+        // Extract relevant data for the chart
+        const labels = data.map((entry) => entry.drug_name);
+        const sideEffects = data.map((entry) => entry.side_effects);
+  
+        // Create a bar chart
+        var ctx = document.getElementById("myChart").getContext("2d");
+        var myChart = new Chart(ctx, {
+          type: "bar",
+          data: {
+            labels: labels,
+            datasets: [
+              {
+                label: "Side Effects",
+                data: sideEffects,
+                backgroundColor: "rgba(75, 192, 192, 0.2)",
+                borderColor: "rgba(75, 192, 192, 1)",
+                borderWidth: 1,
+              },
+            ],
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true,
+              },
+            },
+          },
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  });
