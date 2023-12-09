d425623a3eb87ef518c48e4585eef37efb7210be
UzmaSayyeda <uzmafatima5991@gmail.com>
2023-12-09 01:18:27 -0600

    added completed plots

--- flask-model/static/js/index.js
+++ flask-model/static/js/index.js
@@ -1,72 +1,56 @@
 let response = '/data'
 
+
 d3.json(response).then((data) => {
 
-    let drugNames = data.map((entry) => entry.drug_name)
-    // console.log(drugNames)
+    // get rx_otc from /data
+    let rxOtc = data.map((entry) => entry.rx_otc)
 
-    let x = drugNames.slice(0,10).reverse()
-   // console.log(x)
+    let x = rxOtc
 
-    let medicalCond = data.map((entry) => entry.medical_condition)
-    // console.log(medicalCond)
+    // plot histogram with rx_otc
+    let DataRx = [
 
-    let y = medicalCond.slice(0,10).reverse()
-    // console.log(y)
-
-    let barData = [
-
-        {   x : x,
-            y : y,
-            type : "bar",
-            
-        
+        {
+            x : x,        
+            type : "histogram"
+         
         }
     ];
 
-    let barLayout = {
-        title : "Top ten drugs"
+    // set up layout
+    let LayoutRx = {
+        title : "Rx OTC"
 
     };
-
-    Plotly.newPlot("myChart", barData, barLayout)
+    // display 
+    Plotly.newPlot("view-2", DataRx, LayoutRx)
 })
 
+d3.json(response).then((data) => {
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     fetch("/data")
-//       .then((response) => response.json())
-//       .then((data) => {
-//         // Extract relevant data for the chart
-//         const labels = data.map((entry) => entry.drug_name);
-//         const sideEffects = data.map((entry) => entry.side_effects);
-  
-//         // Create a bar chart
-//         var ctx = document.getElementById("myChart").getContext("2d");
-//         var myChart = new Chart(ctx, {
-//           type: "bar",
-//           data: {
-//             labels: labels,
-//             datasets: [
-//               {
-//                 label: "Side Effects",
-//                 data: sideEffects,
-//                 backgroundColor: "rgba(75, 192, 192, 0.2)",
-//                 borderColor: "rgba(75, 192, 192, 1)",
-//                 borderWidth: 1,
-//               },
-//             ],
-//           },
-//           options: {
-//             scales: {
-//               y: {
-//                 beginAtZero: true,
-//               },
-//             },
-//           },
-//         });
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching data:", error);
-//       });
-//   });
+    // get medical_condition and map 
+    let medicalCond = data.map((entry) => entry.medical_condition)
+
+    let  x2 = medicalCond
+
+    // plot the medical condtition histogram
+    let DataMed = [
+
+        {
+            x : x2,            
+            type : "histogram"
+         
+        }
+    ];
+
+    // define the layout
+    let LayoutMed = {
+        title : "Medical Condition"
+
+    };
+    
+    // display using html tag id, bar data and layout
+    Plotly.newPlot("view-3", DataMed, LayoutMed)
+})
+ 
